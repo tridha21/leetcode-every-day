04900694b982471eda100aaf4678e7e16c1a8711
Tridha Chand <tridhac210@gmail.com>
2026-09-02 23:59:18 +0530

    Time: 0 ms (100%), Space: 45.2 MB (35.83%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -1,6 +1,16 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-
         return true;
+    //    There are only two cases:
+
+    //    All numbers have the same parity → just keep every number as it is.
+    //         [2, 4, 6] → all even ✅
+    //         [1, 3, 5] → all odd ✅
+    //         There are both odd and even numbers → subtract a number of the opposite parity.
+    //         Even − Odd = Odd
+    //         Odd − Even = Odd
+    //         So we can make every element odd. ✅
+    //         Therefore, for every valid input, the answer is always true
+
     }
 }
